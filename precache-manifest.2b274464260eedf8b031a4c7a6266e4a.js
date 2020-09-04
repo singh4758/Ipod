806523f72758fcec74350871c25a706a7c42bc8f
Abhishek Singh <singh4758@gmail.com>
2020-09-04 22:48:59 +0530

    Updates

---
+++ precache-manifest.2b274464260eedf8b031a4c7a6266e4a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3daa198e9cc5e3afcb71ee7cea7ffc1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe27e0e71c814014022",
+    "url": "/static/css/main.06c98d1a.chunk.css"
+  },
+  {
+    "revision": "f0aed2805181cb05bf07",
+    "url": "/static/js/2.54133cd7.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/static/js/2.54133cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe27e0e71c814014022",
+    "url": "/static/js/main.f826846f.chunk.js"
+  },
+  {
+    "revision": "9079cd80d2926db20796",
+    "url": "/static/js/runtime-main.8b885632.js"
+  },
+  {
+    "revision": "f58a59fabd76a1dfe6ee9f67879ef19c",
+    "url": "/static/media/Laung Gwacha.f58a59fa.mp3"
+  },
+  {
+    "revision": "13b23bf93a624c63329e4c8435d741a2",
+    "url": "/static/media/Let Me Love You.13b23bf9.mp3"
+  },
+  {
+    "revision": "42cb592fe70552deba360bc6b8554101",
+    "url": "/static/media/Shape of you.42cb592f.mp3"
+  },
+  {
+    "revision": "ab7c69c2c47e3cbd52c55486514eb1d3",
+    "url": "/static/media/earth.ab7c69c2.jpg"
+  },
+  {
+    "revision": "ec42ead05165b16b9d0c011d0966c4c2",
+    "url": "/static/media/game.ec42ead0.jpg"
+  },
+  {
+    "revision": "494ed9c0922a3ee87ef2569f5b29d8fe",
+    "url": "/static/media/jupiter.494ed9c0.png"
+  },
+  {
+    "revision": "a4fb5517ac4ae3f854a509769d1c5ef3",
+    "url": "/static/media/laung_gwacha.a4fb5517.jpg"
+  },
+  {
+    "revision": "afb8461f9cdaa8339e73d5be2439e454",
+    "url": "/static/media/moon.afb8461f.jpg"
+  },
+  {
+    "revision": "5d8a23839f98efec957c488457eea93e",
+    "url": "/static/media/settings.5d8a2383.png"
+  },
+  {
+    "revision": "220ba5c7b2ca6df36e1c8dc54c43e0fb",
+    "url": "/static/media/shape_of_you.220ba5c7.png"
+  }
+]);
